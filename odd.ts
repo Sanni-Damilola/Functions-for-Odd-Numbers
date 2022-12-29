7e3335e7d9a328e6ab6ff64215dbe071c90c22d6
Sanni_Damilola <114070063+Sanni-Damilola@users.noreply.github.com>
2022-12-29 17:11:10 +0100

    Add files via upload

---
+++ odd.ts
@@ -0,0 +1,26 @@
+const odd = (value : number[]) => {
+  let hold: number[] = [];
+
+  // value.forEach((el) => {
+  //   if (el % 2 !== 0) {
+  //     hold.push(el);
+  //   }
+  // });
+
+  // value.forEach((el) => {
+  //   if (el % 2 !== 0) {
+  //     hold.push(el)
+  //   }
+  // })
+  // for (let i = 0; i <= value; i++) {
+  //   if(i % 2 !== 0) {
+  //     hold.push(i)
+  //   }
+  // }
+
+  // return hold;
+
+  return hold;
+}
+
+console.log(odd([1,2,3,4,5,67,8,]));
